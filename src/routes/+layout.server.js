921122e4891419e7d2d8de27d10fc4de944899ec
nerevarinee <rouabhi-rauf@tuta.io>
2026-05-11 11:16:10 +0100

    done frontend 3

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -6,3 +6,7 @@ import { dev } from '$app/environment';
 import { injectAnalytics } from '@vercel/analytics/sveltekit';
 
 injectAnalytics({ mode: dev ? 'development' : 'production' });
+
+import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+injectSpeedInsights();
